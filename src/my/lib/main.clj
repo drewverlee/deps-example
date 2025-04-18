f66c1bbdd52b39288cdf4a5c4c37350f0310731f
drew verlee <drew.verlee@gmail.com>
2025-04-18 07:35:01 -0500

    add a time delay example.

--- src/my/lib/main.clj
+++ src/my/lib/main.clj
@@ -4,8 +4,11 @@
 (defn get-time [] (java.util.Date.))
 (def time-def (get-time))
 (defmacro time-macro [] time-def)
+(defonce time-def-delay (delay time-def))
 
+(println "hello")
 (println "time-def: " time-def)
 (println  "time-macro:" (time-macro))
+(println "time-def-delay: " @time-def-delay)
 
 (defn -main [& args])
